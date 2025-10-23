0592988ef6b9976389587ab2e5b65ea0972e2992
bmonagan <bernardmonagan@gmail.com>
2025-10-23 17:47:21 -0400

    Setting up skeleton for main.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def main():
-    print("Hello from calculator!")
-
+    print("Calculator, Press Q to quit")
+    while True:
+        equation = input("Enter Equation:")
+        if equation.lower() == "q":
+            print("Closing program")
+            break
 
 if __name__ == "__main__":
     main()
