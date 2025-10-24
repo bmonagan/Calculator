49f5db754cd9b193b8720207b4fe79c8f83783f5
bmonagan <bernardmonagan@gmail.com>
2025-10-23 23:26:14 -0400

    Adding some more to main. seems pretty simple. want to add gui which would make this something completly different.

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+from calculations import calculate
 def main():
     print("Calculator, Press Q to quit")
+    prev = None 
     while True:
         equation = input("Enter Equation:")
         if equation.lower() == "q":
             print("Closing program")
             break
+        if prev:
+            output = equation + prev
+            calculate(output)
+        else:
+            output = calculate(equation)
+
+        
+        prev = output
+        print(output)
+
 
 if __name__ == "__main__":
     main()
+2
