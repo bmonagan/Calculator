3ab1466df423c175c6b02ba870b5c015288c0233
bmonagan <bernardmonagan@gmail.com>
2025-10-25 08:21:31 -0400

    alignment

--- gui_test.py
+++ gui_test.py
@@ -10,11 +10,11 @@ def number_print(number):
     DISPLAY_NUMBER *= 10
     DISPLAY_NUMBER += number
 ttk.Label(frm, text=f"{DISPLAY_NUMBER}").grid(column=0, row=0)
-ttk.Button(frm, text="1", command=lambda: number_print(1)).grid(column=1, row=0)
+ttk.Button(frm, text="1", command=lambda: number_print(1)).grid(column=0, row=1)
 ttk.Button(frm, text="2", command=lambda: number_print(2)).grid(column=1, row=1)
-ttk.Button(frm, text="3", command=lambda: number_print(3)).grid(column=1, row=2)
-ttk.Button(frm, text="4", command=lambda: number_print(4)).grid(column=2, row=0)
-ttk.Button(frm, text="5", command=lambda: number_print(5)).grid(column=2, row=1)
+ttk.Button(frm, text="3", command=lambda: number_print(3)).grid(column=2, row=1)
+ttk.Button(frm, text="4", command=lambda: number_print(4)).grid(column=0, row=2)
+ttk.Button(frm, text="5", command=lambda: number_print(5)).grid(column=1, row=2)
 ttk.Button(frm, text="6", command=lambda: number_print(6)).grid(column=2, row=2)
 
 root.mainloop()
