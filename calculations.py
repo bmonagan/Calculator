eb5d144c9b6919b402abc50b04a968e305c17731
bmonagan <bernardmonagan@gmail.com>
2025-11-04 01:01:44 -0500

    Trying a different structure. Not sure how im going to handle moving the numbers right now but should be okay.

--- calculations.py
+++ calculations.py
@@ -25,4 +25,4 @@ def division():
 def backspace():
     pass
 def equals():
-    
+    pass
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from calculations import calculate, append_digit
+import calculations
 import tkinter as tk
 
 
@@ -28,28 +28,27 @@ def main():
         ('1', 5, 0), ('2', 5, 1), ('3', 5, 2),
         ('0', 6, 1)
     ]
-    
     for (digit, row, col) in buttons:
         tk.Button(
             frm, 
             text=digit,
             width=5,
             height=2,
-            command=lambda d=digit: append_digit(current_number, d)
+            command=lambda d=digit: calculations.append_digit(current_number, d)
         ).grid(row=row, column=col, padx=2, pady=2)
     
     function_buttons = [
-        ('<x|', 2, 0), ('AC', 2, 1), ('-', 2, 2),
-        ('/', 3,   3), ('x', 4, 3 ), ('+', 5, 3),
+        ('<x|', 2, 0, calculations.backspace), ('AC', 2, 1, calculations.clear), ('-', 2, 2, calculations.subtraction),
+        ('/', 3,   3, calculations.division), ('x', 4, 3 , calculations.multiplication), ('+', 5, 3, calculations.addition),
         
     ]
-    for (digit, row, col) in function_buttons:
+    for (digit, row, col,function) in function_buttons:
         tk.Button(
             frm, 
             text=digit,
             width=5,
             height=2,
-            command=lambda d=digit: append_digit(current_number, d)
+            command=function
         ).grid(row=row, column=col, padx=2, pady=2)
 
     root.mainloop()
