9399628b05dffc2af132ca326bb1818ff4162952
bmonagan <bernardmonagan@gmail.com>
2025-11-07 18:56:21 -0500

    reordering numbers on the calculator to add more space and properly align buttons.

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ def main():
     # Secondary and main display variables and labels
     current_number = tk.StringVar()
     label = tk.Label(frm, textvariable=current_number, font=('Arial', 24), anchor='e', width=12)
-    label.grid(column=0, row=1, columnspan=3, padx=5, pady=10, sticky='ew')
+    label.grid(column=0, row=1, columnspan=4, padx=5, pady=10, sticky='ew')
     current_number.set("0") 
 
     secondary_number = tk.StringVar()
     secondary_label = tk.Label(frm, textvariable=secondary_number, font=('Arial', 12), anchor='e', width=12)
-    secondary_label.grid(column=0, row=0, columnspan=3, padx=5, sticky='ew')
+    secondary_label.grid(column=0, row=0, columnspan=4, padx=5, sticky='ew')
     secondary_number.set("")
 
     def format_display(value):
@@ -48,13 +48,13 @@ def main():
         update_display(current_number, current_value=new)
 
     # Create number buttons in a grid (calculator layout)
-    buttons = [
-        ('7', 3, 0), ('8', 3, 1), ('9', 3, 2),
-        ('4', 4, 0), ('5', 4, 1), ('6', 4, 2),
-        ('1', 5, 0), ('2', 5, 1), ('3', 5, 2),
-        ('0', 6, 1)
+    digit_buttons = [
+        ('7', 4, 0), ('8', 4, 1), ('9', 4, 2),
+        ('4', 5, 0), ('5', 5, 1), ('6', 5, 2),
+        ('1', 6, 0), ('2', 6, 1), ('3', 6, 2),
+        ('0', 7, 1)
     ]
-    for (digit, row, col) in buttons:
+    for (digit, row, col) in digit_buttons:
         tk.Button(
             frm, 
             text=digit,
@@ -65,15 +65,15 @@ def main():
     
     function_buttons = [
         # (digit, row, col, function_reference, [args])
-        ('<x|', 2, 0, calculations.backspace, [current_number]), 
+        ('⌫',2, 3, calculations.backspace, [current_number]), 
         ('AC', 2, 1, calculations.clear, [current_number, secondary_number]), 
-        ('-', 2, 2, calculations.subtraction, [current_number, secondary_number]),
-        ('/', 3, 3, calculations.division, [current_number, secondary_number]), 
-        ('x', 4, 3 , calculations.multiplication, [current_number, secondary_number]), 
+        ('-', 4, 3, calculations.subtraction, [current_number, secondary_number]),
+        ('/', 4, 1, calculations.division, [current_number, secondary_number]), 
+        ('x', 4, 2 , calculations.multiplication, [current_number, secondary_number]), 
         ('+', 5, 3, calculations.addition, [current_number, secondary_number]),
-        ('+/-', 6,0, calculations.negation, current_number),
-        ('.', 6, 2, calculations.decimal_point, [current_number]),
-        ('=', 6, 3, calculations.equals, [current_number, secondary_number]),
+        ('+/-', 7,0, calculations.negation, current_number),
+        ('.', 7, 2, calculations.decimal_point, [current_number]),
+        ('=', 7, 3, calculations.equals, [current_number, secondary_number]),
         
     ]
     for (digit, row, col, func_ref, args) in function_buttons:
